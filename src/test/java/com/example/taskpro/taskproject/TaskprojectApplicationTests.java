b6f5e01b341aa27c68fb439f2bc2f2ad7dd59339
githubprash <prashantbj.30@gmail.com>
2022-07-05 10:47:02 +0530

    Initilisation of Project

---
+++ src/test/java/com/example/taskpro/taskproject/TaskprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.taskpro.taskproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
